9461aa9284b8ffd614cb5eb715bb5be102ab4b06
Sebastian <72226894+Shawson97@users.noreply.github.com>
2021-02-23 21:50:28 +0100

    Add files via upload

--- sourcefinder/settings.py
+++ sourcefinder/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'sourcefinder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/tmp/mysite.sqlite3',,
     }
 }
 
